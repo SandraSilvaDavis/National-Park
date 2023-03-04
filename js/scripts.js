7c3b24266473bc747b6228e9e64f90162d75d0e9
Sandra Davis <sandra.silva.davis@gmail.com>
2023-03-03 23:41:01 -0700

    place holders added , changes

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//inject current year in footer 
+const rightNow = new Date();
+console.log(rightNow.getFullYear())
+document.querySelector('#year').textContent = rightNow.getFullYear();
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+
+}
+    const x = document.getElementById("hamburguerBtn");
+
+    x.onclick = toggleMenu;
